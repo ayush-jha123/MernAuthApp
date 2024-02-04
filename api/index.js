62df342016b75eeb1cff1408bccc34444e370719
AyushJha <kumarayushjha123@gmail.com>
2024-02-04 11:32:34 +0530

    Connected To mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to Mongodb');
+}).catch((err)=>{
+    console.log(`Error:${err}`);
+})
 
 const app=express();
 
