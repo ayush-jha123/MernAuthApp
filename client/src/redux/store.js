e398b7ce2e292396c0dfd06b04b07c9fda56efaa
AyushJha <kumarayushjha123@gmail.com>
2024-02-10 10:06:11 +0530

    Redux persist added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,22 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from './user/userSLice';
+import {persistReducer,persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';//This save our data inside localo storage of browser
+
+const rootReducer=combineReducers({user:userReducer})
+const persistConfig={
+    key:'root',
+    version:1,
+    storage,
+}
+
+const persistedReducer=persistReducer(persistConfig,rootReducer);
+
 export const store=configureStore({
-    reducer:{user:userReducer},
+    reducer:persistedReducer,
     middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
         serializableCheck:false,
     })
 })
+
+export const persistor=persistStore(store)
